083b4af0297e07fe8af7ab0af575ef24a29d7fd1
alexoserna <118146045+alexoserna@users.noreply.github.com>
2022-12-01 18:19:40 -0500

    completed javascript acitivities

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,18 @@
 // Write Your JavaScript Code Here
+
+var personName = "Alexis";
+var pets = 1;
+var funFact = "I have built a guitar";
+
+// Reassign
+/*
+personName = "Bob";
+pets = 0;
+funFact = "My daughter's name is Donna"
+*/
+
+// Console.log messages
+
+console.log("My namne is " + personName + ".");
+console.log("I have " + pets + " pet(s)");
+console.log("Fun fact: " + funFact);
