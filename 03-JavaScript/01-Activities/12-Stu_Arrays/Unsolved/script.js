84cc59e3bb5664db4be1c5e35d06c96d990b3dc6
alexoserna <118146045+alexoserna@users.noreply.github.com>
2022-12-01 19:06:03 -0500

    completed activity 12

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,16 @@
 // WRITE YOUR CODE HERE
+var studentNames = ["Bob", "Joey", "Dylan", "Alex", "Maria"];
+
+console.log(studentNames.length);
+
+for ( x = 0; x < studentNames.length; x++) {
+    console.log( "Welcome to the class " + studentNames[x] );
+}
+
+var newStudent = "Ben";
+
+studentNames[0] = newStudent;
+
+if ( studentNames[0] = newStudent ) {
+    console.log( newStudent + " is in class" );
+}
