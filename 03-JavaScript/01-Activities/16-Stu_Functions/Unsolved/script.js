2c73f5d2f22a2701a26cdc507af08636f5b6fdc0
alexoserna <118146045+alexoserna@users.noreply.github.com>
2022-12-01 21:33:37 -0500

    completed scope activity

--- 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
+++ 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
@@ -16,7 +16,20 @@ isEqual(10, 10);
 // Refer to sample functions as needed!
 
 // TODO: Call the isEqual function so that it logs "They are equal in value"
+isEqual(10, "10");
+isEqual(10, 2);
 
 // TODO: Rewrite isEqual as a function expression called 'isEqualTakeTwo`
+var isEqualTake2 = function(x, y) {
+  if (x === y) {
+    console.log('They are equal in type and value');
+  } else if (x == y) {
+    console.log('They are equal in value');
+  } else {
+    console.log('They are not equal');
+  }
+  return;
+}
 
 // TODO: Call the isEqualTakeTwo function so that it logs "They are not equal"
+isEqualTake2(2, 10);
