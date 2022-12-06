b201b79b8f6dc1571cf1cb1708d3a7ee87265ee1
alexoserna <118146045+alexoserna@users.noreply.github.com>
2022-12-05 19:20:03 -0500

    activity 22 done

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,14 @@
 //WRITE YOUR CODE BELOW
+var customerOrder = {
+    name : "drink",
+    numSugars : 2,
+    ready : true
+}
+
+console.log(customerOrder.name);
+console.log(customerOrder.numSugars);
+if (customerOrder.ready) {
+    console.log("Ready for pickup");
+} else {
+    console.log("Still in order queue");
+}
